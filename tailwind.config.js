68096f24bbd1a6d7d0d8be1c8b6d84899ccf3a57
RafiAbiyyu23 <rafiabiyyu735@gmail.com>
2023-12-20 14:21:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins' : ['Poppins', 'sans-serif'],
+      'teko' : ['Teko', 'sans-serif'],
+    },
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
